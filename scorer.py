b649d7b02b9c71c314841457b6570eb45fb0b70d
Aydinli, Mehmet-Fazil (BSH) <Mehmet-Fazil.Aydinli@bshg.com>
2020-06-10 11:29:06 +0300

    use only public leaderboard, update scoring

--- scorer.py
+++ scorer.py
@@ -9,11 +9,12 @@ class Scorer():
         self.private_path = private_path
         self.metric = metric
 
-        self.df_public_key = pd.read_csv(self.public_path)
-        self.df_private_key = pd.read_csv(self.private_path)
+        self.df_public_key = pd.read_csv(self.public_path, header=None)
+        self.df_private_key = pd.read_csv(self.private_path, header=None)
         
     def calculate_score(self, submission_path, submission_type = 'public'):
-        df_submission = pd.read_csv(submission_path)
+        #df_submission = pd.read_csv(submission_path)
+        df_submission = pd.read_csv(submission_path, header=None)
 
         if submission_type == 'private':
             df_key = self.df_private_key
@@ -25,19 +26,22 @@ class Scorer():
             print(len(df_key), len(df_submission))
             return ("NOT SAME LENGTH", None)
         
-        df_merged = df_key.merge(df_submission, how ='inner', 
-                                left_on='data_id', right_on='data_id', # adjust `on` columns as params
-                                suffixes=('_key', '_submission')) 
+        #df_merged = df_key.merge(df_submission, how ='inner', 
+        #                        left_on='data_id', right_on='data_id', # adjust `on` columns as params
+        #                        suffixes=('_key', '_submission')) 
         # When submission and key have different index value
-        if len(df_key) != len(df_merged):
-            return ("NOT SAME INDEX", None)
+        #if len(df_key) != len(df_merged):
+        #    return ("NOT SAME INDEX", None)
 
         # data size is same, time to get score
-        y_key = df_merged['prediction_key']
-        y_submission = df_merged['prediction_submission']
+        #y_key = df_merged['prediction_key']
+        #y_submission = df_merged['prediction_submission']
 
-        if y_submission.isna().sum() > 0:
-            return ("SUBMISSION HAS NULL VALUE", None)
+        y_key = df_key[0]
+        y_submission = df_submission[0]
+
+        #if y_submission.isna().sum() > 0:
+        #    return ("SUBMISSION HAS NULL VALUE", None)
 
         score = self.metric(y_key, y_submission)
         return ("SUBMISSION SUCCESS", score)
